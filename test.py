50c2243f0237659c93f5fb897f36a09b0f627bcd
kalyani bhujbal <kalyanibhujbal1203@gmail.com>
2024-07-27 21:15:43 +0530

    python basics program

---
+++ test.py
@@ -0,0 +1,13 @@
+
+user_inputs = []
+
+
+for i in range(10):
+   
+    user_input = input(f"Enter input {i + 1}: ")
+   
+    user_inputs.append(user_input)
+
+
+print("The inputs are:")
+print(user_inputs)
